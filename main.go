9e0e888b849e6138aab2ab55250b547e8b28d9e1
onishikotaro0024 <onishi.kotaro.0024@gmail.com>
2026-08-22 07:46:49 +0900

    Improve age check logic and add repeat function
    Refactor age check and add repeat function for counting.

--- main.go
+++ main.go
@@ -1,22 +1,26 @@
-package main 
+package main
+
 // パッケージ宣言
 
 import "fmt"
+
 // fmt パッケージ宣言
 
 func main() {
 	fmt.Println("Hello, Go!")
-	
+
 	name()
-	
+
 	greet("Kotaro")
 	greet("ABC")
-	
+
 	result := add(10, 20)
 	fmt.Println(result)
 
 	checkAge(30)
 	checkAge(18)
+
+	repeat()
 }
 
 func name() {
@@ -36,13 +40,23 @@ func add(a int, b int) int {
 	return a + b
 }
 
-
 func checkAge(age int) {
-	if age >= 65{
+	if age >= 65 {
 		fmt.Println("シニアです")
-		}else if age >= 20{
+	} else if age >= 20 {
 		fmt.Println("成人です")
-		}else{
+	} else {
 		fmt.Println("未成年です")
-		}
+	}
+}
+
+func repeat() {
+	total := 0
+
+	for i := 1; i <= 5; i++ {
+		fmt.Println(i)
+		total += i
+	}
+
+	fmt.Println("合計:", total)
 }
