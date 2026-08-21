25707220d7f96d268da927757ff064fecbf6570c
onishikotaro0024 <onishi.kotaro.0024@gmail.com>
2026-08-22 07:06:58 +0900

    add[name]

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
-package main
+package main 
+// パッケージ宣言
 
 import "fmt"
+// fmt パッケージ宣言
+
 
 func main() {
 	fmt.Println("Hello, Go!")
+}
+
+
+
+func name() {
+	name := "Kotaro"
+age := 30
+
+fmt.Println(name)
+fmt.Println(age)
 }
