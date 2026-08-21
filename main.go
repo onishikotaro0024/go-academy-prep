05b4d5e4e87a84a4df5fcb7ea9b3319b5ee54101
onishikotaro0024 <onishi.kotaro.0024@gmail.com>
2026-08-22 07:32:41 +0900

    Modify age in name function and implement checkAge
    Update age variable in name function and add checkAge function.

--- main.go
+++ main.go
@@ -14,14 +14,18 @@ func main() {
 	
 	result := add(10, 20)
 	fmt.Println(result)
+
+	checkAge(30)
+	checkAge(18)
 }
 
 func name() {
 	name := "Kotaro"
-	age := 30
+	age := 27
 
 	fmt.Println(name)
 	fmt.Println(age)
+
 }
 
 func greet(name string) {
@@ -30,4 +34,15 @@ func greet(name string) {
 
 func add(a int, b int) int {
 	return a + b
+}
+
+
+func checkAge(age int) {
+	if age >= 65{
+		fmt.Println("シニアです")
+		}else if age >= 20{
+		fmt.Println("成人です")
+		}else{
+		fmt.Println("未成年です")
+		}
 }
