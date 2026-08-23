87cba0f3dd85d776293ccf2be16aba548686d5eb
onishikotaro0024 <onishi.kotaro.0024@gmail.com>
2026-08-24 03:29:24 +0900

    Comment out functions in calc.go
    Comment out the add and divide functions in calc.go

--- calc.go
+++ calc.go
@@ -1,15 +1,15 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func add(a int, b int) int {
-	return a + b
-}
+// func add(a int, b int) int {
+// 	return a + b
+// }
 
-func divide(a int, b int) (int, error) {
-	if b == 0 {
-		return 0, fmt.Errorf("0では割れません")
-	}
+// func divide(a int, b int) (int, error) {
+// 	if b == 0 {
+// 		return 0, fmt.Errorf("0では割れません")
+// 	}
 
-	return a / b, nil
-}
+// 	return a / b, nil
+// }
