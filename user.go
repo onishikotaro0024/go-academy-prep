887199a467dbf05d88f11d19b2ec71913df71e0a
onishikotaro0024 <onishi.kotaro.0024@gmail.com>
2026-08-24 03:29:37 +0900

    Update user.go

--- user.go
+++ user.go
@@ -1,21 +1,21 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-type User struct {
-	Name string
-	Age  int
-	Job  string
-}
+// type User struct {
+// 	Name string
+// 	Age  int
+// 	Job  string
+// }
 
-func (u User) Introduce() {
-	fmt.Println("こんにちは、", u.Name, "です")
-}
+// func (u User) Introduce() {
+// 	fmt.Println("こんにちは、", u.Name, "です")
+// }
 
-func (u User) GetJob() string {
-	return u.Job
-}
+// func (u User) GetJob() string {
+// 	return u.Job
+// }
 
-func (u *User) ChangeJob(newJob string) {
-	u.Job = newJob
-}
+// func (u *User) ChangeJob(newJob string) {
+// 	u.Job = newJob
+// }
