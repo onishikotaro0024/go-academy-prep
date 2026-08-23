35965c91a832e3fb917d59751aa7aa1f9beec4bb
onishikotaro0024 <onishi.kotaro.0024@gmail.com>
2026-08-24 03:41:06 +0900

    Restore arithmetic functions and add error checking
    Restored add and divide functions, and added checkNumber function for error handling.

--- calc.go
+++ calc.go
@@ -2,21 +2,24 @@ package main
 
 import "fmt"
 
-// func add(a int, b int) int {
-// 	return a + b
-// }
-
-// func divide(a int, b int) (int, error) {
-// 	if b == 0 {
-// 		return 0, fmt.Errorf("0では割れません")
-// 	}
-
-// 	return a / b, nil
-// }
-
-
 func add(a int, b int) int {
 	return a + b
+}
+
+func divide(a int, b int) (int, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("0では割れません")
+	}
+
+	return a / b, nil
+}
+
+func checkNumber(n int) error {
+	if n < 0 {
+		return fmt.Errorf("マイナスの値です")
+	}
+
+	return nil
 }
 
 func repeatSum() {
@@ -28,33 +31,4 @@ func repeatSum() {
 	}
 
 	fmt.Println("合計:", total)
-}
-
-func scopeTest() {
-name := "Kotaro"
-total := 0
-
-for i := 1; i <= 3; i++ {
-	fmt.Println(i)
-	total += i
-	}
-fmt.Println(name)
-fmt.Println("合計:", total)
-	
-}
-
-func divide(a int, b int) (int, error) {
-		if b == 0 {
-			return 0, fmt.Errorf("0では割れません")
-	}
-			return a / b, nil
-}
-
-
-
-func checkNumber(n int) error{
-		if n < 0 {
-			return fmt.Errorf("マイナスの値です")
-	}
-			return nil
 }
