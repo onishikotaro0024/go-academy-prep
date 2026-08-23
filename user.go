b763a5cfbcf4bc3ca208248d8aa8705c70aef205
onishikotaro0024 <onishi.kotaro.0024@gmail.com>
2026-08-24 03:26:10 +0900

    Implement User struct with methods
    Added User struct with methods for introduction and job management.

--- user.go
+++ user.go
@@ -1,1 +1,21 @@
+package main
 
+import "fmt"
+
+type User struct {
+	Name string
+	Age  int
+	Job  string
+}
+
+func (u User) Introduce() {
+	fmt.Println("こんにちは、", u.Name, "です")
+}
+
+func (u User) GetJob() string {
+	return u.Job
+}
+
+func (u *User) ChangeJob(newJob string) {
+	u.Job = newJob
+}
