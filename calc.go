7874e159be2d202e33851969e01d5d8ab8f91396
onishikotaro0024 <onishi.kotaro.0024@gmail.com>
2026-08-24 03:36:00 +0900

    Update calc.go

--- calc.go
+++ calc.go
@@ -1,6 +1,6 @@
-// package main
+package main
 
-// import "fmt"
+import "fmt"
 
 // func add(a int, b int) int {
 // 	return a + b
@@ -13,3 +13,48 @@
 
 // 	return a / b, nil
 // }
+
+
+func add(a int, b int) int {
+	return a + b
+}
+
+func repeatSum() {
+	total := 0
+
+	for i := 1; i <= 5; i++ {
+		fmt.Println(i)
+		total += i
+	}
+
+	fmt.Println("合計:", total)
+}
+
+func scopeTest() {
+name := "Kotaro"
+total := 0
+
+for i := 1; i <= 3; i++ {
+	fmt.Println(i)
+	total += i
+	}
+fmt.Println(name)
+fmt.Println("合計:", total)
+	
+}
+
+func divide(a int, b int) (int, error) {
+		if b == 0 {
+			return 0, fmt.Errorf("0では割れません")
+	}
+			return a / b, nil
+}
+
+
+
+func checkNumber(n int) error{
+		if n < 0 {
+			return fmt.Errorf("マイナスの値です")
+	}
+			return nil
+}
