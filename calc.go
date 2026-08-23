e872aac028964f466226c293091c99b0767901fe
onishikotaro0024 <onishi.kotaro.0024@gmail.com>
2026-08-24 03:27:09 +0900

    Add add and divide functions to calc.go

--- calc.go
+++ calc.go
@@ -1,1 +1,15 @@
+package main
 
+import "fmt"
+
+func add(a int, b int) int {
+	return a + b
+}
+
+func divide(a int, b int) (int, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("0では割れません")
+	}
+
+	return a / b, nil
+}
