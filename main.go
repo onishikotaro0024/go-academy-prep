de3ebb6b7db6d3a6b1327354ba7329cccda414b7
onishikotaro0024 <onishi.kotaro.0024@gmail.com>
2026-08-24 03:25:25 +0900

    Add main function with user introduction and addition

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
+package main
 
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Go!")
+
+	user := User{
+		Name: "Kotaro",
+		Age:  27,
+		Job:  "Designer",
+	}
+
+	user.Introduce()
+
+	result := add(10, 20)
+	fmt.Println(result)
+}
