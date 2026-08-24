1c9c5b4fc6c6a46ab1b736cc0e17370c9ff4f1cf
onishikotaro0024 <onishi.kotaro.0024@gmail.com>
2026-08-24 17:03:33 +0900

    Refactor Divide function with error handling
    Refactored Divide function to use checkZero for zero division error handling.

--- calc/calc.go
+++ calc/calc.go
@@ -6,10 +6,52 @@ func Add(a int, b int) int {
 	return a + b
 }
 
-func Divide(a int, b int) (int, error) {
-	if b == 0 {
-		return 0, fmt.Errorf("0では割れません")
-	}
+// func Divide(a int, b int) (int, error) {
+// 	if b == 0 {
+// 		return 0, fmt.Errorf("0では割れません")
+// 	}
 
+// 	return a / b, nil
+// }
+
+// func checkZero(b int) error {
+// 	if b == 0 {
+// 		return fmt.Errorf("0では割れません")
+// 	}
+
+// 	return nil
+// }
+
+
+
+func Divide(a int, b int) (int, error) {
+		err := checkZero(b)
+	if err != nil{
+		return 0, err
+		}
 	return a / b, nil
 }
+
+func checkZero(b int) error {
+	if b == 0{
+		return fmt.Errorf("0では割れません")
+		}
+	return nil
+}
+
+// これでもOK↓//
+
+// func Divide(a int, b int) (int, error) {
+// 	checkZero := func(b int) error {
+// 	if b == 0{
+// 		return fmt.Errorf("0では割れません")
+// 		}
+// 	return nil
+// 	}	
+// 		err := checkZero(b)
+// 	if err != nil{
+// 		return 0, err
+// 		}
+// 	return a / b, nil
+// }
+	
