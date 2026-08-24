87f92f80063c03af5146da8dc267b29f4e82454e
onishikotaro0024 <onishi.kotaro.0024@gmail.com>
2026-08-24 16:27:04 +0900

    Add Divide function with error handling

--- calc.go
+++ calc.go
@@ -3,3 +3,12 @@ package calc
 func Add(a int, b int) int {
 	return a + b
 }
+
+
+func Divide(a int, b int) (int, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("0では割れません")
+	}
+
+	return a / b, nil
+}
